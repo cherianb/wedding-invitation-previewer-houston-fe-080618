e93c8d3d0b4c96d919325d3b2581b8866c5574fe
Cherian Porter <cherianbelun@gmail.com>
2018-10-11 01:49:44 +0000

    automatically backed up by learn

--- wedding.js
+++ wedding.js
@@ -3,8 +3,16 @@ document.getElementById("Formal").addEventListener(‘click’, function() {
 let container=document.querySelector('.flex-container2');
 container.style.backgroundImage=“url(’https://cdn.pixabay.com/photo/2018/03/13/04/52/flower-3221695_960_720.jpg')”
 
+document.getElementById("Spring").addEventListener(‘click’, function() {
+let container=document.querySelector('.flex-container2');
+container.style.backgroundImage=“url(‘https://www.xmple.com/wallpaper/grid-graph-paper-cyan-green-1920x1080-c2-10b6ad-20b2aa-l2-10-360-a-15-f-20.svg’)”
+})
 
 
+document.getElementById("Art Deco").addEventListener(‘click’, function() {
+let container=document.querySelector('.flex-container2');
+container.style.backgroundImage=“url(‘https://www.xmple.com/wallpaper/grid-graph-paper-cyan-green-1920x1080-c2-10b6ad-20b2aa-l2-10-360-a-15-f-20.svg’)”
+})
 
 
 
