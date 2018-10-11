aca94024f848450bd3773e97e379284258f2fe2a
Cherian Porter <cherianbelun@gmail.com>
2018-10-11 01:29:02 +0000

    automatically backed up by learn

--- wedding.js
+++ wedding.js
@@ -1,4 +1,8 @@
-
+document.addEventListener(‘DOMContentLoaded’, function(){
+document.getElementById(‘invite-1’).addEventListener(‘click’, function() {
+let container=document.querySelector(‘.flex-2’);
+container.style.backgroundImage=“url(’https://cdn.pixabay.com/photo/2018/03/13/04/52/flower-3221695_960_720.jpg')”
+})
 
 
 
